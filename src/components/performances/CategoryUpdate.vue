b64bd90d683db728fd0bed4346ce2260ea8f89c6
김도연 <kdymay93@naver.com>
2020-07-26 18:24:08 +0900

    account-edit

--- src/components/performances/CategoryUpdate.vue
+++ src/components/performances/CategoryUpdate.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-        <b-form-group label="">
+        <!-- <b-form-group label="">
         <b-form-checkbox-group id="checkbox-group-2" v-model="updatecategories" name="flavour-2">
             <b-form-checkbox :value="1" @click="inputcategory">1</b-form-checkbox>
             <b-form-checkbox :value="2" @click="inputcategory">2</b-form-checkbox>
@@ -11,8 +11,38 @@
             <b-form-checkbox :value="7" @click="inputcategory">7</b-form-checkbox>
             <b-form-checkbox :value="8" @click="inputcategory">8</b-form-checkbox>
         </b-form-checkbox-group>
-        </b-form-group>        
-      <button @click="changecategory">관심 카테고리 정보 수정</button>
+        </b-form-group>         -->
+      <!-- <button @click="changecategory">관심 카테고리 정보 수정</button> -->
+          <div class="form-group">
+            <b-form-group>
+            <b-form-checkbox-group buttons button-variant="info" size="sm" v-model="updatecategories">
+                <b-form-checkbox class="category-button m-2" :value="1" @click="inputcategory">음악/콘서트</b-form-checkbox>
+            </b-form-checkbox-group>
+            <b-form-checkbox-group buttons button-variant="info" size="sm" v-model="updatecategories">
+                <b-form-checkbox class="category-button  m-2" :value="2" @click="inputcategory">뮤지컬/오페라</b-form-checkbox>
+            </b-form-checkbox-group>
+            <b-form-checkbox-group buttons button-variant="info" size="sm" v-model="updatecategories">
+                <b-form-checkbox class="category-button  m-2" :value="3" @click="inputcategory">연극</b-form-checkbox>
+            </b-form-checkbox-group>
+            <b-form-checkbox-group buttons button-variant="info" size="sm" v-model="updatecategories">
+                <b-form-checkbox class="category-button m-2" :value="4" @click="inputcategory">국악</b-form-checkbox>
+            </b-form-checkbox-group>
+            <b-form-checkbox-group buttons button-variant="info" size="sm" v-model="updatecategories">
+                <b-form-checkbox class="category-button m-2" :value="5" @click="inputcategory">무용/발레</b-form-checkbox>
+            </b-form-checkbox-group>
+            <b-form-checkbox-group buttons button-variant="info" size="sm" v-model="updatecategories">
+                <b-form-checkbox class="category-button m-2" :value="6" @click="inputcategory">아동/가족</b-form-checkbox>
+            </b-form-checkbox-group>
+            <b-form-checkbox-group buttons button-variant="info" size="sm" v-model="updatecategories">
+                <b-form-checkbox class="category-button m-2" :value="7" @click="inputcategory">전시</b-form-checkbox>
+            </b-form-checkbox-group>
+            <b-form-checkbox-group buttons button-variant="info" size="sm" v-model="updatecategories">
+                <b-form-checkbox class="category-button m-2" :value="8" @click="inputcategory">기타</b-form-checkbox>
+            </b-form-checkbox-group>
+            </b-form-group>
+          </div>
+      <button class="btn btn-outline-primary" @click="changecategory">관심 장르 수정</button>
+
   </div>
 </template>
 
--- src/views/accounts/UserProfileUpdateView.vue
+++ src/views/accounts/UserProfileUpdateView.vue
@@ -1,29 +1,80 @@
 <template>
-<div class="container">
-    <h1 class="title">회원정보수정</h1>
-    <hr>
-  <span v-for="category in categories" :key="category">
-      {{category}}
-  </span>
-  <CategoryUpdate @emitchangcategory="changcategory"/>
-  <hr>
-    <form class="center">
-      <div>
-        <h4>아이디 : {{username}}</h4>
+<div>
+  <div class="container">
+    <form>
+        <h1 class="d-flex">Edit Profile</h1>
+        <hr>
+        <div class="row">
+            <!-- left column -->
+            <div class="col-md-3">
+              <div class="text-center">
+                <div class="form-group">
+                    <!-- <img v-if="!flag" class="profile" :src="show_image" alt="profile_image"> -->
+                    <b-avatar v-if="!flag" :src="show_image" size="10rem"></b-avatar>
+                    <!-- <img v-if="flag" class="profile" :src="change_image" alt="profile_image"> -->
+                    <b-avatar  v-if="flag" :src="change_image" size="10rem"></b-avatar>
+                    <br>
+                    <hr>
+                    <h6 class="d-flex">프로필 수정</h6>
+                    <input type="file" id="file" ref="file" @change="imgUpload()"/>
+                </div>
+              </div>
+            </div>
+
+                <!-- edit form column -->
+            <div class="col-md-7">
+              <h3 class="d-flex">Personal info</h3>
+              <hr>
+              <br>
+              <!-- Change this to a <form> to reproduce your original issue -->
+              <div role="form">
+                <div class="form-group d-flex flex-row">
+                  <label class="col-lg-3 control-label">아이디</label>
+                  <div class="col-lg-8 d-flex flex-row">
+                    <h5>{{username}}</h5>
+                    <PasswordUpdate class="ml-auto"/>
+                  </div>
+                </div>
+
+                <!-- <div class="form-group d-flex flex-row">
+                  <label class="col-lg-3 control-label">비밀번호</label>
+                  <div class="col-lg-8">
+                    <PasswordUpdate/>
+                  </div>
+                </div> -->
+
+                <div class="form-group d-flex flex-row">
+                  <label class="col-lg-3 control-label">email</label>
+                  <div class="col-lg-8">
+                    <input type="email" class="form-control" id="email" v-model="email">
+                  </div>
+                </div>
+                
+                <div class="form-group d-flex flex-row">
+                  <label class="col-lg-3 control-label">관심장르</label>
+                    <div class="col-lg-8 container">
+                        <div class="d-flex justify-content-center">
+                          <div v-for="category in categories" :key="category" class="m-2">
+                              <h4><span class="badge badge-secondary">{{category}}</span></h4>
+                          </div>
+                        </div>
+                          <div class="collapse" id="collapseExample">
+                            <div class="card card-body">
+                              <CategoryUpdate @emitchangcategory="changcategory"/>   
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <button class="btn btn-outline-primary d-flex flex-row-reverse align-self-end" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+            관심장르변경
+          </button>
       </div>
-      <br>
       <hr>
-      <div class="form-group">
-          <label for="exampleFormControlFile1">프로필 이미지 수정</label>
-          <input type="file" id="file" ref="file" @change="imgUpload()"/>
-      </div>
-      <div class="form-group">
-        <label for="email">email 수정</label>
-        <input type="email" class="form-control" id="email" v-model="email">
-      </div>
-      <button type="submit" class="btn btn-primary" @click="img_email_update">회원정보 수정</button>
+      <button type="submit" class="btn btn-primary d-flex flex-row ml-auto" @click="img_email_update">회원정보 수정</button>
     </form>
-<PasswordUpdate/>
+  </div>
 </div>
 </template>
 
@@ -42,6 +93,9 @@ export default {
   },
   data(){
     return {
+        flag:false,
+        show_image:'',
+        change_image:'',
         config:'',
         user_id:'',
         categories:'',
@@ -82,7 +136,8 @@ export default {
         .then((reaponse)=>{
           this.getcategory()
           this.email = reaponse.data.data.email
-          this.profile_image = reaponse.data.data.profile_image
+          this.show_image = BACK_URL+reaponse.data.data.profile_image
+          console.log(this.show_image)
           this.username = reaponse.data.data.username
         })
         .catch((err)=>{
@@ -92,20 +147,24 @@ export default {
     img_email_update(event) {
       event.preventDefault()
       let formData = new FormData();
-      formData.append('profile_image', this.profile_image)
+      if (this.$refs.file.files[0]!==undefined){
+        formData.append('profile_image', this.profile_image)
+      }
       formData.append('email', this.email)
       axios.put(`${BACK_URL}/accounts/`+ this.$route.params.ID +'/', formData, this.config)
           .then(() => {
             this.$alert("유저정보가 수정되었습니다.")
-            // this.$router.push({name:'Home'})
+            this.$router.push({ name: 'UserDetailView', params: { userId: this.$route.params.ID }})
           })
           .catch((error) => {
           console.log(error)
-          this.$alert("수정 할 이미지가 없습니다.")
+          this.$alert("다시 입력 해 주세요.")
           })
     },
     imgUpload(){
-    this.profile_image = this.$refs.file.files[0];
+      this.profile_image = this.$refs.file.files[0];
+      this.change_image = URL.createObjectURL(this.profile_image)
+      this.flag = true
     },
     changcategory(data){
         this.categories=data.data.category
